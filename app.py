cbdf020b3d30475f0134d52d56551f6cb014e776
24f20093155 <24f2009315@gmail.com>
2025-09-20 15:58:51 +0530

    Milestone-HMS DB-Relationship

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from application.models import db,Admin
+from werkzeug.security import generate_password_hash
+
+def create_app():
+    app=Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///model.db"
+    app.config["SECRET_KEY"]="fgwiigfvssyughgtytfyhj"
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+        if not Admin.query.filter_by(username="vaishnavi").first():
+            admin=Admin(name = "Vaishnavi" , username = "vaishnavi" , password=generate_password_hash("vaishnavi"),role="admin")
+            db.session.add(admin)
+            db.session.commit()
+    return app
+
+app=create_app()
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run()
---
+++ application/models.py
@@ -0,0 +1,64 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Admin(db.Model):
+    __tablename__ = "admin"
+
+    id=db.Column(db.Integer(),primary_key=True,autoincrement=True)
+    username = db.Column(db.String(),unique=True,nullable=False)
+    password = db.Column(db.String(),nullable=False)
+    name = db.Column(db.String(),nullable=False)
+    role = db.Column(db.String(),nullable=False)
+
+class Doctor(db.Model):
+    __tablename__ = "doctor"
+
+    id=db.Column(db.Integer(),primary_key=True,autoincrement=True)
+    name = db.Column(db.String(),nullable=False)
+    specialization=db.Column(db.String(),nullable=False)
+    availability=db.Column(db.String())
+    email=db.Column(db.String(),unique=True,nullable=False)
+    password=db.Column(db.String(),nullable=False)
+    department_id=db.Column(db.Integer(),db.ForeignKey("department.id"))
+    role = db.Column(db.String(),nullable=False)
+
+class Patient(db.Model):
+    __tablename__ = "patient"
+
+    id=db.Column(db.Integer(),primary_key=True,autoincrement=True)
+    name=db.Column(db.String(),nullable=False)
+    age=db.Column(db.Integer(),nullable=False)
+    gender=db.Column(db.String())
+    contact=db.Column(db.String(),unique=True,nullable=False)
+    email=db.Column(db.String(),unique=True,nullable=False)
+    password=db.Column(db.String(),nullable=False)
+    address=db.Column(db.String())
+    role = db.Column(db.String(),nullable=False)
+
+class Appointment(db.Model):
+    __tablename__ = "appointment"
+
+    id = db.Column(db.Integer(),primary_key=True,autoincrement=True)
+    patient_id = db.Column(db.Integer() , db.ForeignKey("patient.id") ,nullable=False)
+    doctor_id = db.Column(db.Integer() , db.ForeignKey("doctor.id"),nullable=True)
+    date = db.Column(db.Date()  ,nullable = False)
+    time = db.Column(db.Time(),nullable = False)
+    status = db.Column(db.String(),nullable = False , default="Booked")
+
+class Treatment(db.Model):
+    __tablename__ = "treatment"
+
+    id=db.Column(db.Integer(),primary_key=True,autoincrement=True)
+    appointment_id = db.Column(db.Integer(),db.ForeignKey("appointment.id"),nullable=False)
+    diagnosis = db.Column(db.String(),nullable = False)
+    prescription = db.Column(db.String(),nullable = False)
+    notes = db.Column(db.String())
+
+class Department(db.Model):
+    __tablename__ = "department"
+
+    id = db.Column(db.Integer(),primary_key = True,autoincrement=True)
+    department_name = db.Column(db.String(),nullable = False,unique=True)
+    description = db.Column(db.String(),nullable = False)
+    doctors_registered = db.Column(db.String())
